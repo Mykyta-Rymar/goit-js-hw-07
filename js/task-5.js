566a1dcf0d558c5b60e1bd1970756506c63ebc4a
Mykyta_Rymar <mykitarymar@gmail.com>
2025-07-02 03:55:01 +0200

    task 5 fertig

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const hopa = document.querySelector(".change-color");
+const spam = document.querySelector(".widget-color");
+const bodyFive = document.querySelector("body");
+
+hopa.addEventListener("click", aboba);
+
+function aboba() {
+  const color = getRandomHexColor();
+  bodyFive.style.backgroundColor = color;
+  spam.textContent = color;
+}
